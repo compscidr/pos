b70584a3ce9b915d14d38b0e732e40f56e17aa74
Jason Ernst <jason@left.io>
2018-09-16 16:35:00 -0700

    added the bind udp function call, fixed receptions of packets which arrive quickly after the initial broadcast so that if the listen function hasn't had time to complete it will still be received correctly.

--- src/net/udp.c
+++ src/net/udp.c
@@ -5,10 +5,16 @@
 #include "mm.h"
 #include "mutex.h"
 
-#define MAX_PORTS 1024
+#define MAX_PORTS   1024
+#define UDP_BUFFER  1024
 
-#define PORT_FREE - 1
-#define PORT_LISTEN 0
+enum {
+  PORT_FREE,
+  PORT_BIND,
+  PORT_LISTEN,
+  DATA_READY
+};
+
 
 //if -1 port is free, otherwise the size of the last operation
 int ports[MAX_PORTS] = {
@@ -16,7 +22,7 @@ int ports[MAX_PORTS] = {
 }; 
 
 //temp buffer for copying
-char buffer[1024] = {
+char buffer[UDP_BUFFER] = {
   0
 };
 
@@ -46,6 +52,10 @@ void udp_init() {
     ports[i] = PORT_FREE;
 }
 
+/*
+ * TODO: buffer for each process, make each buffer a cyclical buffer
+ * and only overwrite data when it has been pushed to the process
+ */
 void udp_receive_packet(char * data, unsigned short length) {
   char temp[40];
 
@@ -55,30 +65,39 @@ void udp_receive_packet(char * data, unsigned short length) {
   }
 
   struct udp_packet_header packet;
-  memcpy( & packet, data, sizeof(struct udp_packet_header));
+  memcpy( &packet, data, sizeof(struct udp_packet_header));
 
-  //print_string("UDP packet received ");
-  //print_string(itoa(length,temp,10));
-  //print_string(" bytes from port: ");
-  //print_string(itoa(ntohs(packet.source_port), temp, 10));
-  //print_string(" to port: ");
-  //print_string(itoa(ntohs(packet.destination_port), temp, 10));
-  //print_string("\n");
+  /*
+  print_string("UDP packet received ");
+  print_string(itoa(length,temp,10));
+  print_string(" bytes from port: ");
+  print_string(itoa(ntohs(packet.source_port), temp, 10));
+  print_string(" to port: ");
+  print_string(itoa(ntohs(packet.destination_port), temp, 10));
+  print_string("\n");
+  */
 
   //only continue if the port is correct
   int port = ntohs(packet.destination_port);
   if (port > 0 && port < MAX_PORTS) {
     //check if the port is even listening before proceeding
-    if (ports[port] == PORT_LISTEN) {
+    if (ports[port] != PORT_FREE) {
+      if(ports[port] == DATA_READY) {
+        print_string("PORT HAS DATA READY - PROBABLY GOING TO OVERWRITE STUFF WAITING IN QUEUE FOR PROCESS");
+      }
       //copy the received packet to the buffer
       spinlock();
       memcpy(buffer, 0, 1024);
-      print_string("Copying ");
-      print_string(itoa(length - sizeof(struct udp_packet_header), temp, 10));
-      print_string(" to buffer\n");
+      //print_string("Copying ");
+      //print_string(itoa(length - sizeof(struct udp_packet_header), temp, 10));
+      //print_string(" to buffer\n");
       memcpy(buffer, data + sizeof(struct udp_packet_header), length - sizeof(struct udp_packet_header));
       spinunlock();
       ports[ntohs(packet.destination_port)] = length - sizeof(struct udp_packet_header);
+    } else {
+      print_string("Not listening on UDP port: ");
+      print_string(itoa(ntohs(packet.destination_port), temp, 10));
+      print_string("\n");
     }
   }
 }
@@ -94,9 +113,39 @@ void udp_broadcast(unsigned char * data, unsigned short length, unsigned short s
   udp.checksum = 0x0000; //initially zero until we compute the checksum
 
   char * buffer = calloc(length + sizeof(struct udp_packet_header));
-  memcpy(buffer, & udp, sizeof(struct udp_packet_header));
+  memcpy(buffer, &udp, sizeof(struct udp_packet_header));
   memcpy(buffer + sizeof(struct udp_packet_header), data, length);
+  
+  //print_string("DUMPING UDP PACKET: \n");
+  //hd((unsigned long int)buffer, (unsigned long int)buffer + length + sizeof(struct udp_packet_header));
+  
   ipv4_broadcast(buffer, length + sizeof(struct udp_packet_header), 17);
+  //print_string("UDP Broadcast done\n");
+}
+
+int udp_bind(unsigned short port) {
+  char temp[33] = {0};
+  print_string("UDP Binding to port: ");
+  print_string(itoa(port, temp, 10));
+  print_string("\n");
+  
+  //check if the port is already busy
+  if (ports[port] == PORT_FREE) {
+    ports[port] = PORT_BIND;
+    return 1;
+  }
+  else {
+    print_string("PORT TAKEN ALREADY");
+    return -1;
+  }  
+}
+
+int udp_close(unsigned short port) {
+  if(port > MAX_PORTS) {
+    return -1;
+  }
+  ports[port] = PORT_FREE;
+  return 1;
 }
 
 /*
@@ -105,27 +154,26 @@ void udp_broadcast(unsigned char * data, unsigned short length, unsigned short s
  * to handle the data
  */
 int udp_listen(unsigned short port, char * data, int length) {
-
-  //check if the port is already busy
-  if (ports[port] == PORT_FREE)
+  if(ports[port] == PORT_BIND) {
     ports[port] = PORT_LISTEN;
-  else
-    return -1;
-
+  }
+  
   //make sure we don't overflow buffer
-  if (length > 1024)
+  if (length > UDP_BUFFER) {
+    print_string("UDP Buffer Overflow\n");
     return -1;
+  }
 
   //sleep while waiting for data on the port
   while (ports[port] == PORT_LISTEN) {
-    //__asm__("hlt"); 
+    __asm__("hlt"); 
   }
 
   spinlock();
   memcpy(data, buffer, length);
   int size = ports[port];
-  ports[port] = PORT_FREE;
+  ports[port] = PORT_LISTEN;
   spinunlock();
 
-  return size;;
+  return size;
 }
