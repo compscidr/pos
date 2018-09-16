6759b4ffd02dea2056d03d1e8e58957537551d23
Jason Ernst <jason@left.io>
2018-09-16 16:27:47 -0700

    added a bind and close function to udp to help with delivery of packets that come arrive before the listen call occurs

--- src/include/net/udp.h
+++ src/include/net/udp.h
@@ -4,6 +4,8 @@
 void udp_init(void);
 void udp_receive_packet(char * data, unsigned short length);
 void udp_broadcast(unsigned char * data, unsigned short length, unsigned short source_port, unsigned short destination_port);
+int udp_bind(unsigned short port);
 int udp_listen(unsigned short port, char * data, int length);
+int udp_close(unsigned short port);
 
 #endif
